bc34b403763702d0053a6ca26957ea2d2f8c34a5
samarpitah <samarpita@2pirad.com>
2024-08-08 15:44:11 -0700

    initial commit

---
+++ js/plugins.js
@@ -0,0 +1,52 @@
+
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  arguments.callee = arguments.callee.caller;  
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+};
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+
+
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+function performSearch() {
+	if($('#searchString').val() != 'Search...') {
+		window.location = '/searchResult.html?searchString=' + $('#searchString').val();
+	}
+}
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(unescape(hash[0]));
+        vars[hash[0]] = unescape(hash[1]);
+    }
+    return vars;
+}
+
+
+function equalHeight(group) {
+	var tallest = 0;
+	group.each(function() {
+		var thisHeight = $(this).height();
+		if(thisHeight > tallest) {
+			tallest = thisHeight;
+		}
+	});
+	group.height(tallest);
+}
+
+
+$('#searchString').keypress(function(e){
+    if(e.which == 13 && $('#searchString').val().length > 0){
+    	performSearch();
+     }
+});
